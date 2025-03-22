bea30943426af35f8bcd3ba8a00432d7c8827c5e
rishimalhan <rmalhan0112@gmail.com>
2025-03-22 00:30:34 -0400

    making vnc work

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+Xvfb :1 -screen 0 1920x1200x24 &
+x11vnc -display :1 -forever -nopw -listen localhost -xkb &
+websockify --web=/usr/share/novnc/ 8080 localhost:5900 &
 # Source the setup file
 source /opt/ros/$ROS_DISTRO/setup.bash
 sleep infinity
